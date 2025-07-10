40bfbeb6bb4968da247b2f8d47ea516b3b64f81b
Hilmar Wiegand <me@hwgnd.de>
2025-07-10 23:25:54 +0200

    Add favicons and nix package

--- flake.nix
+++ flake.nix
@@ -4,17 +4,26 @@
   outputs = {nixpkgs, ...}: let
     system = "x86_64-linux";
     pkgs = import nixpkgs {inherit system;};
+    nativeBuildInputs = with pkgs; [zola just tailwindcss_4];
   in {
+    packages.${system}.default = pkgs.stdenv.mkDerivation {
+      inherit nativeBuildInputs;
+      name = "blog";
+      src = ./.;
+
+      buildPhase = ''just build'';
+      installPhase = ''cp -r public $out'';
+    };
+
     devShells.${system}.default = pkgs.mkShell {
-      packages = with pkgs; [
-        zola
-        just
-        process-compose
-        tailwindcss_4
-        watchman
-        biome
-        prettierd
-      ];
+      packages =
+        nativeBuildInputs
+        ++ (with pkgs; [
+          watchman
+          biome
+          prettierd
+          process-compose
+        ]);
     };
   };
 }
